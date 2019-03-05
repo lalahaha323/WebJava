36cb06f9c3e8dfd53bb6bbd914913089d5d9488a
lalahaha323 <2665687186@qq.com>
2019-03-05 20:50:28 +0800

    使用复选框等的一个实例

---
+++ src/lala/haha5/Pet.java
@@ -0,0 +1,36 @@
+package lala.haha5;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet("/pet")
+public class Pet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>感谢填写</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("联系人: <a herf = 'mailto:" +
+                request.getParameter("email") + "'>" +
+                request.getParameter("user") + "</a>");
+        out.println("<br>喜爱的宠物类型");
+        out.println("<ul>");
+        for(String type : request.getParameterValues("type")) {
+            out.println("<li>" + type + "</li>");
+        }
+        out.println("</ul>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+}
