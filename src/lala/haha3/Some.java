8a52d29a1898ff18828d143febff6f8672494c7e
lalahaha323 <2665687186@qq.com>
2019-03-05 19:47:00 +0800

    将另一个servlet用include包含进来，来看一情况的一个实例

---
+++ src/lala/haha3/Some.java
@@ -0,0 +1,21 @@
+package lala.haha3;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@WebServlet("/some.view")
+public class Some extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Some do one...");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("other.view");
+        dispatcher.include(req, resp);
+        out.println("Some do two...");
+        out.close();
+    }
+}
