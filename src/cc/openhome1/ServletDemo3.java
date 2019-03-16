4f848a56983442af02bfe882801b47f30703253a
lalahaha323 <2665687186@qq.com>
2019-03-16 21:04:12 +0800

    ServletContext测试用例2

--- src/cc/openhome1/ServletDemo3.java
+++ src/cc/openhome1/ServletDemo3.java
@@ -1,5 +1,6 @@
 package cc.openhome1;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,16 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/2.do")
-public class aboutServlet2 extends HttpServlet {
-    protected void process(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        String client_randomNum = request.getParameter("randomNum");
-        String session_randomNum = (String) request.getSession().getAttribute("randomNum");
-        if(client_randomNum == null || session_randomNum == null || !client_randomNum.equals(session_randomNum)) {
-            System.out.println("验证码错误了！！！");
-            return ;
-        }
-
+@WebServlet("/Demo3.do")
+public class ServletDemo3 extends HttpServlet {
+    protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        String value = (String) servletContext.getAttribute("Myname");
+        System.out.println(value);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
