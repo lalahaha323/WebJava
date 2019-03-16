85aa6652d904b8a46175ed3c4e7901436301883c
lalahaha323 <2665687186@qq.com>
2019-03-16 21:01:50 +0800

    ServletContext测试用例1

---
+++ src/cc/openhome1/ServletDemo2.java
@@ -0,0 +1,25 @@
+package cc.openhome1;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Demo2.do")
+public class ServletDemo2 extends HttpServlet {
+    protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        String value = "lala";
+        servletContext.setAttribute("Myname" , value);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+}
