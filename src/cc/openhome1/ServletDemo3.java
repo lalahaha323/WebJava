0bf7eb45c45023e8bf1ad3659973793d7131c9ca
lalahaha323 <2665687186@qq.com>
2019-03-16 22:32:37 +0800

    实现ServletConfig

--- src/cc/openhome1/ServletDemo3.java
+++ src/cc/openhome1/ServletDemo3.java
@@ -1,20 +1,32 @@
 package cc.openhome1;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/Demo3.do")
+@WebServlet(
+        name="ServletConfigDemo",
+        urlPatterns = {"/conf"},
+        initParams = {
+                @WebInitParam(name = "PARAM1", value = "VALUE1"),
+                @WebInitParam(name = "PARAM2", value = "VALUE2")
+        }
+)
 public class ServletDemo3 extends HttpServlet {
+    private String PARAM1;
+    private String PARAM2;
+    public void init() throws ServletException {
+        PARAM1 = getServletConfig().getInitParameter("PARAM1");
+        PARAM2 = getServletConfig().getInitParameter("PARAM2");
+    }
+
     protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletContext servletContext = this.getServletContext();
-//        ServletContext servletContext = this.getServletContext();
-        String value = (String) servletContext.getInitParameter("name");
-        System.out.println(value);
+        System.out.println(PARAM1);
+        System.out.println(PARAM2);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
